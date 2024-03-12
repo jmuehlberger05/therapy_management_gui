5172a221433671c36724fd3264f3d6457f755c18
Jakob Mühlberger <jakob@diemuehlbergers.at>
2024-03-12 17:44:44 +0100

    first commit

---
+++ Constants.cs
@@ -0,0 +1,17 @@
+﻿using MySqlX.XDevAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace therapy_management_gui
+{
+    public static class Constants
+    {
+        public const string MYSQL_INSTANCE_HOST = "127.0.0.1:3306";
+        public const string MYSQL_INSTANCE_UID = "root";
+        public const string MYSQL_INSTANCE_PASSWORD = "Emtyuser11";
+        public const string MYSQL_INSTANCE_DATABASE = "therapy_management";
+    }
+}
